c8b95325a9f2f057bdc8ffa8f0b32d260324c803
hyungjoon yoon <hysy9255@gmail.com>
2023-08-15 23:03:23 +0900

    승자 결정하기

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,39 @@ const Board = () => {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState(true);
 
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [3, 4, 5],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `The winner is ${winner}`;
+  } else {
+    status = `Next Player is ${isXNext ? "X" : "O"}`;
+  }
+
   const clickHandler = (i) => {
     const newSquares = squares.slice();
     newSquares[i] = isXNext ? "X" : "O";
@@ -19,7 +52,7 @@ const Board = () => {
 
   return (
     <div>
-      <div className="status">`Next Player: {isXNext ? "X" : "O"}`</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
