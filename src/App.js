75fd7513aefff9f055e877e921f225b6329bc384
hyungjoon yoon <hysy9255@gmail.com>
2023-08-16 17:37:29 +0900

    동작에 대한 기록 저장하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,62 @@
-import './App.css';
-import Board from "./components/Board"
+import "./App.css";
+import Board from "./components/Board";
+import { useState } from "react";
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [3, 4, 5],
+      [0, 4, 8],
+      [2, 4, 6],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+    ];
+
+    for (let index = 0; index < lines.length; index++) {
+      const [a, b, c] = lines[index];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+
+  let status;
+  if (winner) {
+    status = `Winner is ${winner}`;
+  } else {
+    status = `Next Player: ${xIsNext ? "X" : "O"}`;
+  }
+
+  const clickHandler = (i) => {
+    const newSquares = current.squares.slice();
+    if (newSquares[i] || winner) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((prev) => !prev);
+  };
+
   return (
     <div className="game">
+      <div className="status">{status}</div>
       <div className="game-board">
-        <Board/>
-      </div>
-      <div className="game-info">
-        game-info
+        <Board squares={current.squares} onClick={(i) => clickHandler(i)} />
       </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,61 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Board.css";
-import Square from "./Square.js";
+import Square from "./Square";
 
-const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true);
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [3, 4, 5],
-      [0, 4, 8],
-      [2, 4, 6],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-    ];
-
-    for (let index = 0; index < lines.length; index++) {
-      const [a, b, c] = lines[index];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[b] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = `The winner is ${winner}`;
-  } else {
-    status = `Next Player is ${isXNext ? "X" : "O"}`;
-  }
-
-  const clickHandler = (i) => {
-    const newSquares = squares.slice();
-    if (winner || newSquares[i]) {
-      return;
-    }
-    newSquares[i] = isXNext ? "X" : "O";
-    setSquares(newSquares);
-    setIsXNext((prev) => !prev);
-  };
-
+const Board = (props) => {
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => clickHandler(i)} />;
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
   };
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
